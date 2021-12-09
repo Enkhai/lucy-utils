196d3bfd2708a3d83dc8b181b6b12ce9ab58ae2d
Enkhai <aerogtx@gmail.com>
2021-12-10 00:23:08 +0200

    Completed combined reward logging to Tensorboard

--- examples/05_reward_log_tensorboard.py
+++ examples/05_reward_log_tensorboard.py
@@ -6,57 +6,59 @@ from rlgym_tools.sb3_utils.sb3_log_reward import SB3CombinedLogReward  # Useful 
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
 from torch.utils.tensorboard import SummaryWriter
-from multiprocessing.queues import SimpleQueue
+from pathlib import Path
+import json
 
 
+# Our custom callback class will inherit from the BaseCallback
 class RewardLogCallback(BaseCallback):
 
-    def __init__(self, log_dir: str,
-                 reward_names: list,
-                 read_queue: SimpleQueue,
+    def __init__(self, log_dumpfile: str,
+                 reward_names_: list,
                  comment: str = "",
                  verbose=0):
+        # Always run the super constructor first
         super(RewardLogCallback, self).__init__(verbose)
-        # Init stuff
-        # A thread could be useful that reads the file as it is written and
-        # sends new line data into a queue that will be read by the Callback
-        # TODO: Make the thread
-        self.read_queue = read_queue
+
+        self.log_dumpfile = Path(log_dumpfile)
+        # the log_dumpfile is of bin/combinedlogfiles/<index>.txt format, bin is the directory
+        log_dir = str(self.log_dumpfile.parent.parent)
         self.writer = SummaryWriter(log_dir=log_dir, comment=comment)
-        # The reward class names will be read during the combined reward initialization
-        # and passed to the callback to add to scalars
-        self.reward_names = reward_names
-        # Having a list initialized from the start can be faster than constantly
-        # creating a new one at each step
-        self.reward_values = []
+        self.log_dumpfile_io = None
+
+        self.reward_names = reward_names_
+        self.n_episodes = 0
 
     def _on_step(self) -> bool:
-        # Here we will be reading the log txt file to log in Tensorboard
+        if not self.log_dumpfile_io and self.log_dumpfile.exists():
+            self.log_dumpfile_io = open(self.log_dumpfile, "r")
 
-        self.reward_values.clear()
-        # Parse the log file from the queue
-        while not self.read_queue.empty():
-            self.reward_values.append(self.read_queue.get())
-
-        # Sort the rewards and compute an average - TODO
-
-        # Log the rewards as scalars - TODO
-        # example
-        self.writer.add_scalar(tag=self.reward_names[0],
-                               scalar_value=0,
-                               global_step=self.num_timesteps,
-                               new_style=True)  # what is new_style?
+        if self.log_dumpfile_io:
+            line = self.log_dumpfile_io.readline()
+            if line and line != "\n":
+                rewards = json.loads(line)
+                for i in range(len(rewards)):
+                    self.writer.add_scalar(tag="Combined reward/" + reward_names[i],
+                                           scalar_value=rewards[i],
+                                           global_step=self.n_episodes)
+                # self.writer.add_scalars(main_tag="Combined Reward",
+                #                         tag_scalar_dict=dict(zip(self.reward_names, json.loads(line))),
+                #                         global_step=self.n_episodes)
+                self.n_episodes += 1
 
         # _on_step must return a boolean
         # If the boolean is false training is aborted early
         return True
+
+    def _on_training_end(self) -> None:
+        # After training is done close the dumpfile IO
+        self.log_dumpfile_io.close()
 
 
 if __name__ == '__main__':
     # Logs the combined rewards for each episode in a txt file inside a ./bin folder
     # This is useful for studying what the model learns to do best and how it may exploit the rewards
     # to obtain a maximum return
-    # TODO: Create a list of reward class names for the callback
     reward = SB3CombinedLogReward.from_zipped(
         (ConstantReward(), -0.02),
         (EventReward(goal=1, concede=-1), 100),
@@ -69,6 +71,9 @@ if __name__ == '__main__':
         (AlignBallGoal(), 0.15),
         (FaceBallReward(), 0.1)
     )
+
+    # We will use this for logging the rewards to Tensorboard
+    reward_names = [fn.__class__.__name__ for fn in reward.reward_functions]
 
     # The observation space depends on the observation builder
     # By default, the observation builder is the DefaultObs, which returns an unbounded observation space of size 70
@@ -106,8 +111,12 @@ if __name__ == '__main__':
                 verbose=1,
                 device="cpu")
 
-    # TODO: continue studying to find a way to log the different combined rewards
-    #  from the log file in ./bin in Tensorboard
+    # We create a callback
+    # The callback will be added to the learning loop and will repeatedly read the log file of the combined rewards
+    # to print a summary to Tensorboard
+    reward_log_callback = RewardLogCallback(log_dumpfile=reward.dumpfile,
+                                            reward_names_=reward_names,
+                                            verbose=1)
 
     # You can observe the model's performance in Tensorboard by running in a terminal
     # `tensorboard --logdir bin`
@@ -115,7 +124,6 @@ if __name__ == '__main__':
     # If you want to have multiple Tensorboard servers running at the same time you can declare
     # a different port by running
     # `tensorboard --logdir <some_folder> --port <some other port, eg. 6007>`
-    # TODO: implement the callback for logging episode rewards
-    model.learn(total_timesteps=100_000_000)
+    model.learn(total_timesteps=100_000_000, callback=reward_log_callback)
 
     env.close()

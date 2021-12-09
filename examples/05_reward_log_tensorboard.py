790d8aedeae3b883b8aec4a2e1b2ca6eb2f0a8b7
Enkhai <aerogtx@gmail.com>
2021-12-10 01:10:07 +0200

    There already exists a SB3CombinedLogRewardCallback functionality..

--- examples/05_reward_log_tensorboard.py
+++ examples/05_reward_log_tensorboard.py
@@ -2,32 +2,32 @@ import rlgym
 from rlgym.utils.reward_functions.common_rewards import *
 from rlgym.utils.terminal_conditions.common_conditions import GoalScoredCondition, TimeoutCondition
 from rlgym_tools.extra_obs.advanced_stacker import AdvancedStacker
-from rlgym_tools.sb3_utils.sb3_log_reward import SB3CombinedLogReward  # Useful for separate combined reward logging
+# Useful for separate combined reward logging
+from rlgym_tools.sb3_utils.sb3_log_reward import SB3CombinedLogReward, SB3CombinedLogRewardCallback
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
-from torch.utils.tensorboard import SummaryWriter
 from pathlib import Path
 import json
 
 
 # Our custom callback class will inherit from the BaseCallback
+# *The custom callback logs rewards at each episode, the SB3CombinedLogRewardCallback logs average rewards
+# at the end of the rollout...
+# SB3CombinedLogRewardCallback is marginally faster than the custom callback due to it being called at the end
+# of the rollout but the code is ultimately slower
+# SB3CombinedLogRewardCallback also plots smoother rewards due to averaging and sparse logging
 class RewardLogCallback(BaseCallback):
 
     def __init__(self, log_dumpfile: str,
                  reward_names_: list,
-                 comment: str = "",
                  verbose=0):
         # Always run the super constructor first
         super(RewardLogCallback, self).__init__(verbose)
 
         self.log_dumpfile = Path(log_dumpfile)
-        # the log_dumpfile is of bin/combinedlogfiles/<index>.txt format, bin is the directory
-        log_dir = str(self.log_dumpfile.parent.parent)
-        self.writer = SummaryWriter(log_dir=log_dir, comment=comment)
         self.log_dumpfile_io = None
 
         self.reward_names = reward_names_
-        self.n_episodes = 0
 
     def _on_step(self) -> bool:
         if not self.log_dumpfile_io and self.log_dumpfile.exists():
@@ -38,13 +38,7 @@ class RewardLogCallback(BaseCallback):
             if line and line != "\n":
                 rewards = json.loads(line)
                 for i in range(len(rewards)):
-                    self.writer.add_scalar(tag="Combined reward/" + reward_names[i],
-                                           scalar_value=rewards[i],
-                                           global_step=self.n_episodes)
-                # self.writer.add_scalars(main_tag="Combined Reward",
-                #                         tag_scalar_dict=dict(zip(self.reward_names, json.loads(line))),
-                #                         global_step=self.n_episodes)
-                self.n_episodes += 1
+                    self.model.logger.record(key="rewards/" + reward_names[i], value=rewards[i])
 
         # _on_step must return a boolean
         # If the boolean is false training is aborted early
@@ -114,9 +108,7 @@ if __name__ == '__main__':
     # We create a callback
     # The callback will be added to the learning loop and will repeatedly read the log file of the combined rewards
     # to print a summary to Tensorboard
-    reward_log_callback = RewardLogCallback(log_dumpfile=reward.dumpfile,
-                                            reward_names_=reward_names,
-                                            verbose=1)
+    reward_log_callback = SB3CombinedLogRewardCallback(rew_names=reward_names)
 
     # You can observe the model's performance in Tensorboard by running in a terminal
     # `tensorboard --logdir bin`

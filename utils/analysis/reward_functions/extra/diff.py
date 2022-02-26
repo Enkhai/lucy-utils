8f20e57d7ab5028d36ca1ee380ee1be25054fe4c
Enkhai <aerogtx@gmail.com>
2022-02-26 21:15:07 +0200

    Worked on player2ball reward examples

--- utils/analysis/reward_functions/extra/diff.py
+++ utils/analysis/reward_functions/extra/diff.py
@@ -1,6 +1,6 @@
-def diff(reward, gamma=1):
+def diff(reward, negative_slope=1):
     """
-    Potential reward function (not potential-based reward shaping function)\n
+    Difference reward function. Not to be confused with potential-based reward shaping function.
     First values are past rewards, last values are recent rewards
     """
-    return reward[1:] - gamma * reward[:-1]
+    return (reward[1:] - reward[:-1]) * negative_slope

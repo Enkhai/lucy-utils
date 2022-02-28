1180f12bc15e03a058452d915d80ccca2d62915a
Enkhai <aerogtx@gmail.com>
2022-02-28 18:52:55 +0200

    Completed distributed reward example. Kickoff, multiply and sequential rewards remaining.

--- utils/analysis/reward_functions/extra/distribute.py
+++ utils/analysis/reward_functions/extra/distribute.py
@@ -8,11 +8,11 @@ def distribute(rewards, team_idcs, team_spirit=0.3):
 
     team_idcs = np.array(team_idcs, dtype=bool)
     rewards = np.stack(rewards)
-    blue_means = rewards[~team_idcs].mean(1)
-    orange_means = rewards[team_idcs].mean(1)
+    blue_means = rewards[~team_idcs].mean(0)
+    orange_means = rewards[team_idcs].mean(0)
 
-    blue = blue_means[~team_idcs] * team_spirit + rewards[~team_idcs] * (1 - team_spirit) - orange_means[team_idcs]
-    orange = orange_means[team_idcs] * team_spirit + rewards[team_idcs] * (1 - team_spirit) - blue_means[~team_idcs]
+    blue = blue_means * team_spirit + rewards[~team_idcs] * (1 - team_spirit) - orange_means
+    orange = orange_means * team_spirit + rewards[team_idcs] * (1 - team_spirit) - blue_means
 
     final_rewards = np.zeros_like(rewards)
     final_rewards[~team_idcs] = blue

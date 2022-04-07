eaf8f40230574cddc83daac37e3c635a11bebe2a
Enkhai <aerogtx@gmail.com>
2022-04-07 23:01:41 +0300

    Improved Necto observation code

--- experiment/necto_leave1out/obs.py
+++ experiment/necto_leave1out/obs.py
@@ -11,12 +11,12 @@ from rlgym.utils.gamestates import GameState, PlayerData
 
 class NectoObs(ObsBuilder):
     _boost_locations = np.array(BOOST_LOCATIONS)
-    _invert = np.array([1] * 5 + [-1, -1, 1] * 5 + [1] * 4)
-    _norm = np.array([1.] * 5 + [2300] * 6 + [1] * 6 + [5.5] * 3 + [1] * 4)
+    _invert = np.array([1] * 5 + [-1, -1, 1] * 5 + [1] * 4 + [1] * 8 + [1])
+    _norm = np.array([1.] * 5 + [2300] * 6 + [1] * 6 + [5.5] * 3 + [1] * 4 + [1] * 8 + [1])
 
     def __init__(self, n_players=6, tick_skip=8):
         """
-        Returns an observation space of shape 1 batch size, 1 `q` player query + 41 `kv` objects, 32 features
+        Returns an observation space of shape (1 batch size, 1 `q` player query + 41 `kv` objects, 32 features)
 
         Features:
          - 0-5 flags: main player, teammate, opponent, ball, boost
@@ -41,7 +41,6 @@ class NectoObs(ObsBuilder):
         self.boost_timers = None
         self.current_state = None
         self.current_qkv = None
-        self.current_mask = None
         self.tick_skip = tick_skip
 
     def reset(self, initial_state: GameState):
@@ -58,65 +57,66 @@ class NectoObs(ObsBuilder):
         else:
             self.current_state = state
 
-        qkv = np.zeros((1, 1 + self.n_players + len(state.boost_pads), 24))  # Ball, players, boosts
+        qkv = np.zeros((1 + self.n_players + len(state.boost_pads), 33))  # Ball, players, boosts
+        qkv[:, -1] = 1  # key padding mask
 
         # Add ball
         n = 0
         ball = state.ball
-        qkv[0, 0, 3] = 1  # is_ball
-        qkv[0, 0, 5:8] = ball.position
-        qkv[0, 0, 8:11] = ball.linear_velocity
-        qkv[0, 0, 17:20] = ball.angular_velocity
+        qkv[0, 3] = 1  # is_ball
+        qkv[0, 5:8] = ball.position
+        qkv[0, 8:11] = ball.linear_velocity
+        qkv[0, 17:20] = ball.angular_velocity
+        qkv[0, -1] = 0  # key padding mask
 
         # Add players
         n += 1
         demos = np.zeros(self.n_players)  # Which players are currently demoed
         for player in state.players:
             if player.team_num == BLUE_TEAM:
-                qkv[0, n, 1] = 1  # is_teammate
+                qkv[n, 1] = 1  # is_teammate
             else:
-                qkv[0, n, 2] = 1  # is_opponent
+                qkv[n, 2] = 1  # is_opponent
             car_data = player.car_data
-            qkv[0, n, 5:8] = car_data.position
-            qkv[0, n, 8:11] = car_data.linear_velocity
-            qkv[0, n, 11:14] = car_data.forward()
-            qkv[0, n, 14:17] = car_data.up()
-            qkv[0, n, 17:20] = car_data.angular_velocity
-            qkv[0, n, 20] = player.boost_amount
+            qkv[n, 5:8] = car_data.position
+            qkv[n, 8:11] = car_data.linear_velocity
+            qkv[n, 11:14] = car_data.forward()
+            qkv[n, 14:17] = car_data.up()
+            qkv[n, 17:20] = car_data.angular_velocity
+            qkv[n, 20] = player.boost_amount
             #             qkv[0, n, 21] = player.is_demoed
             demos[n - 1] = player.is_demoed  # Keep track for demo timer
-            qkv[0, n, 22] = player.on_ground
-            qkv[0, n, 23] = player.has_flip
+            qkv[n, 22] = player.on_ground
+            qkv[n, 23] = player.has_flip
+            qkv[n, -1] = 0  # key padding mask
             n += 1
 
         # Add boost pads
         n = 1 + self.n_players
         boost_pads = state.boost_pads
-        qkv[0, n:, 4] = 1  # is_boost
-        qkv[0, n:, 5:8] = self._boost_locations
-        qkv[0, n:, 20] = 0.12 + 0.88 * (self._boost_locations[:, 2] > 72)  # Boost amount
+        qkv[n:, 4] = 1  # is_boost
+        qkv[n:, 5:8] = self._boost_locations
+        qkv[n:, 20] = 0.12 + 0.88 * (self._boost_locations[:, 2] > 72)  # Boost amount
         #         qkv[0, n:, 21] = boost_pads
+        qkv[n:, -1] = 0  # key padding mask
 
         # Boost and demo timers
         new_boost_grabs = (boost_pads == 1) & (self.boost_timers == 0)  # New boost grabs since last frame
         self.boost_timers[new_boost_grabs] = 0.4 + 0.6 * (self._boost_locations[new_boost_grabs, 2] > 72)
         self.boost_timers *= boost_pads  # Make sure we have zeros right
-        qkv[0, 1 + self.n_players:, 21] = self.boost_timers
+        qkv[1 + self.n_players:, 21] = self.boost_timers
         self.boost_timers -= self.tick_skip / 1200  # Pre-normalized, 120 fps for 10 seconds
         self.boost_timers[self.boost_timers < 0] = 0
 
         new_demos = (demos == 1) & (self.demo_timers == 0)
         self.demo_timers[new_demos] = 0.3
         self.demo_timers *= demos
-        qkv[0, 1: 1 + self.n_players, 21] = self.demo_timers
+        qkv[1: 1 + self.n_players, 21] = self.demo_timers
         self.demo_timers -= self.tick_skip / 1200
         self.demo_timers[self.demo_timers < 0] = 0
 
         # Store results
         self.current_qkv = qkv / self._norm
-        mask = np.zeros((1, qkv.shape[1]))
-        mask[0, 1 + len(state.players):1 + self.n_players] = 1
-        self.current_mask = mask
 
     def build_obs(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> Any:
         # Autodetect of zeroes bad for Lucy  # ++
@@ -126,30 +126,19 @@ class NectoObs(ObsBuilder):
         invert = player.team_num == ORANGE_TEAM
 
         qkv = self.current_qkv.copy()
-        mask = self.current_mask.copy()
 
         main_n = state.players.index(player) + 1
-        qkv[0, main_n, 0] = 1  # is_main
+        qkv[main_n, 0] = 1  # is_main
         if invert:
-            qkv[0, :, (1, 2)] = qkv[0, :, (2, 1)]  # Swap blue/orange
+            qkv[:, (1, 2)] = qkv[:, (2, 1)]  # Swap blue/orange
             qkv *= self._invert  # Negate x and y values
 
-        q = qkv[0, main_n, :]
-        q = np.expand_dims(np.concatenate((q, previous_action), axis=0), axis=(0, 1))
+        q = qkv[[main_n], :]
+        q[0, -9:-1] = previous_action
         kv = qkv
 
         # With EARLPerceiver we can use relative coords+vel(+more?) for key/value tensor, might be smart
-        kv[0, :, 5:11] -= q[0, 0, 5:11]
+        kv[:, 5:11] -= q[0, 5:11]
+        obs = np.concatenate([q, kv])
 
-        # +++ start
-        # Wrap the query, the key-value and the mask objects into one numpy array
-        kv = np.concatenate((kv,
-                             np.zeros((*kv.shape[:2], 8)),  # + previous action zeroes
-                             mask[..., None]),  # + mask
-                            axis=-1)
-
-        obs = np.concatenate((np.c_[q, [[[0]]]],  # + mask
-                              kv), axis=1)
-
-        return obs.squeeze(0)  # squash batch dimension
-        # +++ end
+        return obs

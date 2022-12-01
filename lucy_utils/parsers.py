d9ab5ca9328df91e04fdb5ae16bdf4620af96304
Enkhai <aerogtx@gmail.com>
2022-12-01 21:49:15 +0200

    Changed mixed parser

--- lucy_utils/parsers.py
+++ lucy_utils/parsers.py
@@ -15,10 +15,11 @@ class MixedAction(ActionParser):
         self.parsers = parsers
         offsets = np.concatenate([[0], np.cumsum(num_parser_players)])
         self.parser_idx = [offsets[i:i + 2] for i in range(len(num_parser_players))]
+        self.action_lengths = [p.get_action_space().shape[0] for p in parsers]
 
     def get_action_space(self) -> gym.spaces.Space:
         return gym.spaces.Tuple([p.get_action_space() for p in self.parsers])
 
     def parse_actions(self, actions: Any, state: GameState) -> Tuple[np.ndarray]:
-        return tuple(p.parse_actions(actions[n[0]:n[1]], state)
-                     for p, n in zip(self.parsers, self.parser_idx))
+        return tuple(p.parse_actions(actions[n[0]:n[1], :l], state)
+                     for p, l, n in zip(self.parsers, self.action_lengths, self.parser_idx))
